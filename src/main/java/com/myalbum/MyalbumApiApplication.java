7994a648cffae037a32e8ba5b5332dbe9ebac962
zeroh0 <umberto1900@naver.com>
2025-12-22 09:42:36 +0900

    init

---
+++ src/main/java/com/myalbum/MyalbumApiApplication.java
@@ -0,0 +1,13 @@
+package com.myalbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyalbumApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyalbumApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myalbum/MyalbumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyalbumApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
