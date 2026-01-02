2e69e95805061c01cab6a67e348b1db844da2eeb
zeroh0 <umberto1900@naver.com>
2026-01-02 14:30:52 +0900

    feat: 회원 에러 코드 정의

---
+++ src/main/java/com/myalbum/member/exception/MemberError.java
@@ -0,0 +1,21 @@
+package com.myalbum.member.exception;
+
+import com.myalbum.common.response.AppError;
+import lombok.Getter;
+
+@Getter
+public enum MemberError implements AppError {
+    MEMBER_NOT_FOUND("0001", "member.not.found"),
+    DUPLICATE_EMAIL("0002", "member.duplicate.email"),
+    DUPLICATE_USERNAME("0003", "member.duplcated.username"),
+    PASSWORD_NOT_MATCH("0004", "member.not.match.password"),
+    ;
+
+    private final String errorCode;
+    private final String errorKey;
+
+    MemberError(String errorCode, String errorKey) {
+        this.errorCode = errorCode;
+        this.errorKey = errorKey;
+    }
+}
