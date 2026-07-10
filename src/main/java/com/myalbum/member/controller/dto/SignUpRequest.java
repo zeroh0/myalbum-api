2b48f2371e3036156c3bf05c83caaefd419a25db
zeroh0 <umberto1900@naver.com>
2026-07-11 06:49:53 +0900

    feat: 회원가입 시 입력 값 제약조건 추가

--- src/main/java/com/myalbum/member/controller/dto/SignUpRequest.java
+++ src/main/java/com/myalbum/member/controller/dto/SignUpRequest.java
@@ -2,6 +2,8 @@ package com.myalbum.member.controller.dto;
 
 import com.myalbum.common.valid.RequiredField;
 import com.myalbum.member.service.dto.SignUpDto;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,15 +11,28 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SignUpRequest {
 
+    @Email(message = "{member.invalid.email}")
     @RequiredField(params = "member.field.email")
     private String email;
 
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?])[A-Za-z\\d!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]{8,24}$",
+            message = "{member.invalid.password}"
+    )
     @RequiredField(params = "member.field.password")
     private String password;
 
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?])[A-Za-z\\d!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]{8,24}$",
+            message = "{member.invalid.password}"
+    )
     @RequiredField(params = "member.field.passwordConfirm")
     private String passwordConfirm;
 
+    @Pattern(
+            regexp = "^[a-z0-9][a-z0-9_-]{1,18}[a-z0-9]$",
+            message = "{member.invalid.username}"
+    )
     @RequiredField(params = "member.field.username")
     private String username;
 
